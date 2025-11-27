0347fffb73da187f6ad97c4fc3f76aa420b94e9c
positive132 <nekit_ich@tutamail.com>
2025-11-27 15:49:53 +0300

    Add files via upload

---
+++ is_even.py
@@ -0,0 +1,8 @@
+import math
+print('Введите число, чтобы узнать его четность:')
+C = int(input())
+def chislo(C):
+    if C % 2 == 0:
+        print('Ваше число четное')
+    elif C % 2 != 0:
+        print('Ваше число нечетное')
