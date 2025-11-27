fa2b0c582e2282b3c86f403acb234649fe46b3c4
positive132 <nekit_ich@tutamail.com>
2025-11-27 21:20:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import temperature
+import is_even
+import sum
+import circle
+def main():
+      circle.main()
+      sum.main()
+      temperature.main() 
+      is_even.main()
+if __name__ == "__main__":
+        main()
