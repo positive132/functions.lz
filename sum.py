5b2799235b5523da2779d84cd8a595dca3e3c4aa
positive132 <nekit_ich@tutamail.com>
2025-11-27 18:21:07 +0300

    sum.py

--- sum.py
+++ sum.py
@@ -3,4 +3,10 @@ S = (input('Введите ваш список для нахождения их 
 def summa(S):
     return sum([float(x) for x in S])
 a = summa(S)  
+
 print('Вот ваша сумма всех чисел с предоставленного списка', a)
+
+def main() :
+    a = summa(S)  
+if __name__ == '__main__' :
+    main()
