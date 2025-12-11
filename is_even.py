105a339cd177432d5593dbae0bd094a9f5567d66
positive132 <nekit_ich@tutamail.com>
2025-12-11 13:52:43 +0300

    is_even.py

--- is_even.py
+++ is_even.py
@@ -1,8 +1,13 @@
 import math
-C = int(input('Введите число, чтобы узнать его четность:'))
-def chislo(C):
+def chislo():
+    C = int(input('Введите число, чтобы узнать его четность:'))
     if C % 2 == 0:
         print('Ваше число четное.')
     elif C % 2 != 0:
         print('Ваше число нечетное.')
 print(chislo(C))        
+def main() :
+     C = int(input('Введите число, чтобы узнать его четность:'))
+    print(chislo(C))      
+if __name__ == '__main__' :
+    main()
