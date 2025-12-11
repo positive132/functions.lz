2404c20058f08d64c911926409d44be2bc2957b8
positive132 <nekit_ich@tutamail.com>
2025-12-11 14:05:17 +0300

    circle.py

--- circle.py
+++ circle.py
@@ -1,21 +1,15 @@
 import math 
-def dlina():
-    R = float(input('Введите радиус круга в сантиметра:'))
+def dlina(R):
     return 3.14 * 2 * R 
-a = dlina(R)    
-print('Длина окружности в см:', a)
-   
+
 def pl(R):
     return 3.14 * R ** 2
-b = pl(R)    
-print('Площадь круга в см:' , b)
 
 def main() :
-    R = float(input())
+    R = float(input('Введите радиус круга в сантиметра:'))
     a = dlina(R)
     b = pl(R)    
+    print('Длина окружности в см:', a)
+    print('Площадь круга в см:' , b)
 if __name__ == '__main__' :
     main()
-
-
-
