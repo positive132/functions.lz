371975d770877995d51c232f16d88572bf7ac78f
positive132 <nekit_ich@tutamail.com>
2025-12-11 14:26:27 +0300

    main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import is_even
 import sum
 import circle
 def main():
-      circle.main()
-      sum.main()
       temperature.main() 
       is_even.main()
+      sum.main()
+      circle.main()
 if __name__ == "__main__":
+
         main()
