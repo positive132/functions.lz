209464a5a20bbf5473a362ab60aab3286e0ea6bf
positive132 <nekit_ich@tutamail.com>
2025-12-11 14:21:07 +0300

    sum.py

--- sum.py
+++ sum.py
@@ -1,15 +1,12 @@
 import math 
-def summa():
+def summa(S):
     S = (input('Введите ваш список для нахождения их суммы:')).split()
     return sum([float(x) for x in S])
-a = summa(S)  
-
-print('Вот ваша сумма всех чисел с предоставленного списка', a)
 
 def main() :
     S = (input()).split()
     a = summa(S)  
+    print('Вот ваша сумма всех чисел с предоставленного списка', a)
 if __name__ == '__main__' :
     main()
-
 
